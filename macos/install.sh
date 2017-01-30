3a56ccd9c3baa58c1edf8f20ec56d78b01d2e184
Frederic Poulin <fredpoulin@gmail.com>
2017-01-30 10:52:41 -0500

    Adaptation to work on linux

--- macos/install.sh
+++ macos/install.sh
@@ -3,5 +3,10 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
-echo "› sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+UNAME=`uname -s`
+
+# Set macOS defaults
+if [ "$UNAME" = "Darwin" ]; then
+    echo "› sudo softwareupdate -i -a"
+    sudo softwareupdate -i -a
+fi
