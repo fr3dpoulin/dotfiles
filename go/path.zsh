2971a061bc5c984aaa2f8e853f931217bab8f5f5
Frederic Poulin <fredpoulin@gmail.com>
2017-01-30 11:40:39 -0500

    Adding go and p4merge local install

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,7 @@
+# Adding go binaries to the PATH
+if [ -d /usr/local/go ]; then
+    export PATH=$PATH:/usr/local/go/bin
+fi
+
 export GOPATH=$PROJECTS/go
 export PATH="$GOPATH/bin:$PATH"
---
+++ p4merge/path.zsh
@@ -0,0 +1,4 @@
+# Adding perforce tool to the PATH
+if [ -d /usr/local/p4v ]; then
+  export PATH=$PATH:/usr/local/p4v/bin
+fi
